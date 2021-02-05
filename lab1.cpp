66f9484a8c900af7a466a6d9b93103f279b86440
meowshi <denlas31@gmail.com>
2021-02-05 17:09:26 +0300

    Change char (t) to exit

--- lab1.cpp
+++ lab1.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
     cout << "Choose...\n";
     cout << "a) 6 times 'a'    b) everything is simple\n";
-    cout << "c) don't choose   q) quit\n";
+    cout << "c) don't choose   t) turn off\n";
     char choice;
-    while (cin >> choice && choice != 'q'){
+    while (cin >> choice && choice != 't'){
         switch (choice){
             case 'a':
             case 'A': choice_a();
