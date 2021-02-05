f808500a6e241e8e33913519025e916c7c14588e
meowshi <denlas31@gmail.com>
2021-02-05 14:24:31 +0300

    First commit

---
+++ lab1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "lab1_lib.h"
+
+using namespace std;
+
+int main(){
+    cout << "Choose...\n";
+    cout << "a) 5 times 'a'    b) everything is simple\n";
+    cout << "c) don't choose   q) quit\n";
+    char choice;
+    while (cin >> choice && choice != 'q'){
+        switch (choice){
+            case 'a':
+            case 'A': choice_a();
+                break;
+            case 'b':
+            case 'B': choice_b();
+                break;
+            case 'c':
+            case 'C': chioce_c();
+                break;
+            default: cout << "Wrong choice!\n";
+        }
+    }
+}
---
+++ lab1_lib.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void choice_a(void);
+void choice_b(void);
+void choice_c(void);
+
+void choice_a(){
+    for (int i = 0; i < 5; i++){
+        std::cout << i + 1 << ". a.\n";
+    }
+}
+
+void choice_b(){
+    std::cout << "Your choice is b.\n";
+}
+
+void chioce_c(){
+    std::cout << "Never choose c, please.\n";
+}
