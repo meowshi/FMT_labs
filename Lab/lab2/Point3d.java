6f135c3c5bd6a1c1f94f95a0a79a6ff1304f6a7a
meowshi <denlas31@gmail.com>
2021-03-11 16:19:49 +0300

    Исправил класс Point3d

--- Lab/lab2/Point3d.java
+++ Lab/lab2/Point3d.java
@@ -1,40 +1,19 @@
 // Класс трехмерной точки
-public class Point3d {
-    // Координата X
-    private double xCoord;
-    // Координата Y
-    private double yCoord;
+public class Point3d extends Point2d {
     // Координата Z
     private double zCoord;
     // Конструктор инициализации
     public Point3d(double x, double y, double z){
-        xCoord = x;
-        yCoord = y;
+        super(x, y);
         zCoord = z;
     }
     // Конструктор по умолчанию
     public Point3d(){
         this(0.0, 0.0, 0.0);
     }
-    // Возвращение координаты X
-    public double getX(){
-        return xCoord;
-    }
-    // Возвращение координаты Y
-    public double getY(){
-        return yCoord;
-    }
     // Возвращение координаты Z
     public double getZ(){
         return zCoord;
-    }
-    // Установка значения координаты X
-    public void setX(double val){
-        xCoord = val;
-    }
-    // Установка значения координаты Y
-    public void setY(double val){
-        yCoord = val;
     }
     // Установка значения координаты Z
     public void setZ(double val){
@@ -44,13 +23,13 @@ public class Point3d {
     // true – соответствующие координаты двух точек совпали
     // false - в противном случае
     public boolean compare(Point3d p){
-        if (this.xCoord == p.xCoord && this.yCoord == p.yCoord && this.zCoord == p.zCoord){
+        if (this.getX() == p.getX() && this.getY() == p.getY() && this.zCoord == p.zCoord){
             return true;
         }
         return false;
     }
     // Возвращает расстояние от точки до точки
     public double distanceTo(Point3d p){
-        return Math.sqrt(Math.pow(this.xCoord - p.xCoord, 2) + Math.pow(this.yCoord - p.yCoord, 2) + Math.pow(this.zCoord - p.zCoord, 2));
+        return Math.sqrt(Math.pow(this.getX() - p.getX(), 2) + Math.pow(this.getY() - p.getY(), 2) + Math.pow(this.zCoord - p.zCoord, 2));
     }
 }
