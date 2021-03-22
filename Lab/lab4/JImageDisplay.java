d404bc2d1fbdb57792221637d6daaf82f05b14eb
meowshi <denlas31@gmail.com>
2021-03-22 14:18:33 +0300

    BRG -> RGB

--- Lab/lab4/JImageDisplay.java
+++ Lab/lab4/JImageDisplay.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class JImageDisplay extends JComponent {
     private BufferedImage bufferedImage;
     public JImageDisplay(int width, int height) {
-        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
+        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         super.setPreferredSize(new Dimension(width, height));
     }
 
